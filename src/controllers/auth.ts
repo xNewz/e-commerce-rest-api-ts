752666843214edc773efbadfc1a8997f3980bee1
xNewz <pargornruasijan@gmail.com>
2024-06-03 22:46:40 +0700

    add: login route

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
-import { hashSync } from "bcrypt";
+import { hashSync, compareSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
 
 export const signup = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
@@ -18,4 +20,26 @@ export const signup = async (req: Request, res: Response) => {
     },
   });
   res.json(user);
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+    throw new Error("User does not exist");
+  }
+
+  if (!compareSync(password, user.password)) {
+    throw new Error("Invalid password");
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET
+  );
+
+  res.json({ user, token });
 };
--- src/secrets.ts
+++ src/secrets.ts
@@ -5,3 +5,4 @@ dotenv.config({
 });
 
 export const PORT = process.env.PORT || 3000;
+export const JWT_SECRET = process.env.JWT_SECRET!;
