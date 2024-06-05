d9733a9794edf8bc8c51faf6c6f8548f84250ceb
xNewz <pargornruasijan@gmail.com>
2024-06-05 14:08:28 +0700

    add: create cart item

---
+++ src/controllers/cart.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { CreateCartSchema } from "../schema/cart";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
+import { Product } from "@prisma/client";
+import { prismaClient } from "..";
+
+export const addItemToCart = async (req: Request, res: Response) => {
+  const validatedData = CreateCartSchema.parse(req.body);
+  let product: Product;
+  try {
+    product = await prismaClient.product.findFirstOrThrow({
+      where: {
+        id: validatedData.productId,
+      },
+    });
+  } catch (err) {
+    throw new NotFoundException(
+      "Product not found",
+      ErrorCode.PRODUCT_NOT_FOUND
+    );
+  }
+  const cart = await prismaClient.cartItem.create({
+    data: {
+      userId: req.user.id,
+      productId: product.id,
+      quantity: validatedData.quantity,
+    },
+  });
+  res.json(cart);
+};
+
+export const deleteItemFromCart = async (req: Request, res: Response) => {};
+
+export const changeQuantity = async (req: Request, res: Response) => {};
+
+export const getCart = async (req: Request, res: Response) => {};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRoutes from "./auth";
 import productsRoutes from "./products";
 import usersRoutes from "./users";
+import cartRouters from "./cart";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/products", productsRoutes);
 rootRouter.use("/users", usersRoutes);
+rootRouter.use("/carts", cartRouters);
 
 export default rootRouter;
